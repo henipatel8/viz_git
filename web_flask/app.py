d30f2c0937ba30eb76a4af5439bb52a687ec127b
Manavadariya <h_manavadariya@ats-hr328-dm03.fc.ca>
2020-03-11 15:25:21 -0400

    end of class

--- web_flask/app.py
+++ web_flask/app.py
@@ -10,6 +10,17 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://admin:123@mongo:27017/movie_cms'
 mongo = PyMongo(app)
 
+
 @app.route('/')
 def index():
-    return 'This is a Flask CMS'
+    return '<h1>This is a Flask CMS</h1>'
+
+
+@app.route('/fetch')
+def fetch():
+    params = {
+        'spider_name': 'movie',
+        'start_requests': True
+    }
+    response = requests.get('http://scrapy:9080/crawl.json', params)
+    return response.text
